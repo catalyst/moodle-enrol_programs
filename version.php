3591e297035fd48a1f486bc7d331daef865c7b1a
Petr Skoda <petr.skoda@openlms.net>
2024-06-29 09:56:56 +0200

    OWORK-1031 dev: prepare 3.1.0-RC1

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->version   = 2024062800;
 $plugin->requires  = 2023100902.00; // 4.3.2
 $plugin->component = 'enrol_programs';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.1.0dev';
+$plugin->release   = 'v3.1.0';
 $plugin->supported = [403, 403];
 
 $plugin->dependencies = [
