d3c478b0e32ff459c4cf9e9f66f78bba5db75aba
Jonathan Garcia <jonathan.garcia@openlms.net>
2024-07-11 22:40:55 -0500

    OWORK-1053: enrol_programs adding missing upgrade step

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 
-$plugin->version   = 2024062800;
+$plugin->version   = 2024071100;
 $plugin->requires  = 2023100902.00; // 4.3.2
 $plugin->component = 'enrol_programs';
 $plugin->maturity  = MATURITY_STABLE;
